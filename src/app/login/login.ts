8f7e2b1ac04ad10c37558a703e7f3de08c8be763
HERRERA38 <herrera38.fh@hotmail.com>
2026-08-12 17:13:55 -0500

    Add Login component with reactive form connected to AuthService

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup;
+  errorMensaje: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: Auth,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    this.authService.login(username, password).subscribe({
+      next: () => {
+        this.router.navigate(['/tareas']);
+      },
+      error: () => {
+        this.errorMensaje = 'Usuario o contraseña incorrectos';
+      },
+    });
+  }
+}
